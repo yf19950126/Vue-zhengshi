3c7db9b81e863303c2049bf2ec177aeb36a0ac7c
fanshuai <31620171+yf19950126@users.noreply.github.com>
2019-07-08 12:01:30 +0800

    Update Product.vue

--- src/pages/Product.vue
+++ src/pages/Product.vue
@@ -210,7 +210,7 @@
             { min: 2, max: 4, message: '长度在 2 到 4 个字符', trigger: 'blur' }
           ],
           phone: [
-            { required: true,message: '请输入正确的手机号', trigger: 'blur', validator: validPhone }//这里需要用到全局变量
+            {message: '请输入正确的手机号', trigger: 'blur', validator: validPhone }//这里需要用到全局变量
           ],
           gain: [
             { required: true, message: '请选择出勤成果', trigger: 'blur' },
